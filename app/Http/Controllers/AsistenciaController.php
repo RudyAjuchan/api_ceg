1f61be4d7a38d38061aba38d33ffa9553933374e
Rudy Ajuchán <rudyajuchansec32@gmail.com>
2023-10-16 05:05:02 -0600

    api ceg listo

--- app/Http/Controllers/AsistenciaController.php
+++ app/Http/Controllers/AsistenciaController.php
@@ -6,6 +6,7 @@ use App\Models\Asistencia;
 use App\Models\Alumno;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class AsistenciaController extends Controller
 {
@@ -91,5 +92,76 @@ class AsistenciaController extends Controller
     public function destroy(Asistencia $asistencia)
     {
         //
+    }
+
+    public function listarAsistenciaAlumno(Request $request){
+        $alumno = DB::table('alumno')->where('gsuite', $request->correo)->first();
+        $asistencia = Asistencia::
+        selectRaw('alumno.nombres, alumno.apellidos, asistencias.tipo, CONCAT(UPPER(SUBSTRING(DATE_FORMAT(asistencias.created_at, "%W %d %M %Y"),1,1)),LOWER(SUBSTRING(DATE_FORMAT(asistencias.created_at, "%W %d %M %Y"),2))) as fecha , DATE_FORMAT(asistencias.created_at, "%H:%i:%s") as hora')
+        ->where('id_alumno', $alumno->carnet)
+        ->join('alumno', 'alumno.carnet', '=', 'asistencias.id_alumno')
+        ->orderBy('created_at')->get();
+        return $asistencia;
+    }
+
+    public function estadisticaAsistencia(Request $request){
+        $alumno = DB::table('alumno')->where('gsuite', $request->correo)->first();
+        $asistencias = Asistencia::
+        selectRaw('COUNT(id) AS asistencias')
+        ->where('tipo', 'entrada')
+        ->whereBetween('created_at', ['2023-07-31', '2023-10-13'])
+        ->where('id_alumno', $alumno->carnet)->first();
+
+        $justificaciones = Asistencia::
+        selectRaw('COUNT(id) AS justificaciones')
+        ->whereRaw('excusa IS NOT NULL')
+        ->whereBetween('created_at', ['2023-07-31', '2023-10-13'])
+        ->where('id_alumno', $alumno->carnet)->first();
+
+        $asistenciasT = round(($asistencias->asistencias/55)*100,0);
+        $justificacionesT = round(($justificaciones->justificaciones/55)*100,0);
+        $inasistenciasT = round(((55-$asistencias->asistencias-$justificaciones->justificaciones)/55)*100,0);
+
+        return compact('asistenciasT', 'justificacionesT', 'inasistenciasT');
+    }
+
+    public function estadisticaHoraEntrada(Request $request){
+        $alumno = DB::table('alumno')->where('gsuite', $request->correo)->first();
+
+        $asistencia = DB::table('asistencias')
+        ->selectRaw('DATE_FORMAT(created_at, "%W") as dia, DATE_FORMAT(created_at, "%H") as hora, DATE_FORMAT(created_at, "%i") as minutos')
+        ->where('tipo', 'entrada')
+        ->where('id_alumno', $alumno->carnet)
+        ->whereBetween('created_at', ['2023-10-02', '2023-10-05'])->get();
+
+        //ALGORITMO PARA OBTENER LUNES Y VIERNES PARA OBTENER DATOS DE UNA SEMANA PARA LA ESTADÍSTICA
+        /* $diaSemana = date("w");
+        # Calcular el tiempo (no la fecha) de cuándo fue el inicio de semana
+        $tiempoDeInicioDeSemana = strtotime("-" . ($diaSemana-1) . " days"); # Restamos -X days
+        $fechaInicioSemana = date("Y-m-d", $tiempoDeInicioDeSemana);
+        $tiempoDeFinDeSemana = strtotime("+" . 4 . " days", $tiempoDeInicioDeSemana);
+        $fechaFinSemana = date("Y-m-d", $tiempoDeFinDeSemana); */
+
+        return $asistencia;
+    }
+
+    public function estadisticaHoraSalida(Request $request){
+        $alumno = DB::table('alumno')->where('gsuite', $request->correo)->first();
+
+        $asistencia = DB::table('asistencias')
+        ->selectRaw('DATE_FORMAT(created_at, "%W") as dia, DATE_FORMAT(created_at, "%H") as hora, DATE_FORMAT(created_at, "%i") as minutos')
+        ->where('tipo', 'salida')
+        ->where('id_alumno', $alumno->carnet)
+        ->whereBetween('created_at', ['2023-10-02', '2023-10-05'])->get();
+
+        //ALGORITMO PARA OBTENER LUNES Y VIERNES PARA OBTENER DATOS DE UNA SEMANA PARA LA ESTADÍSTICA
+        /* $diaSemana = date("w");
+        # Calcular el tiempo (no la fecha) de cuándo fue el inicio de semana
+        $tiempoDeInicioDeSemana = strtotime("-" . ($diaSemana-1) . " days"); # Restamos -X days
+        $fechaInicioSemana = date("Y-m-d", $tiempoDeInicioDeSemana);
+        $tiempoDeFinDeSemana = strtotime("+" . 4 . " days", $tiempoDeInicioDeSemana);
+        $fechaFinSemana = date("Y-m-d", $tiempoDeFinDeSemana); */
+
+        return $asistencia;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AsistenciaController;
+use App\Http\Controllers\NotificacionController;
 use App\Http\Controllers\TokenController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/apiCEG/asistencias', 'AsistenciaController');
 Route::apiResource('/apiCEG/token', 'TokenController');
 Route::post('/apiCEG/listarToken', [TokenController::class, 'getTokenAlumno']);
+Route::post('/apiCEG/listarAsistencias', [AsistenciaController::class, 'listarAsistenciaAlumno']);
+Route::post('/apiCEG/estadisticaAsistencia', [AsistenciaController::class, 'estadisticaAsistencia']); 
+Route::post('/apiCEG/estadisticaHoraEntrada', [AsistenciaController::class, 'estadisticaHoraEntrada']); 
+Route::post('/apiCEG/estadisticaHoraSalida', [AsistenciaController::class, 'estadisticaHoraSalida']); 
+Route::post('/apiCEG/listarNotificaciones', [NotificacionController::class, 'listarNotificacionAlumno']);
+Route::apiResource('/apiCEG/justificar', 'JustificacionController');
 Route::get('/', function () {
     return view('welcome');
 });
